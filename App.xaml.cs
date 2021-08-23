f14af3558bfc7ae7337c87a34aaaa4cff9cef974
Shiki-Himura <athaus@hotmail.de>
2021-08-23 12:30:21 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using GameSearch.ViewModels;
+using GameSearch.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GameSearch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IServiceProvider _serviceProvider;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            CreateServiceProvider();
+
+            _serviceProvider.GetRequiredService<ShellView>().Show();
+        }
+
+        private void CreateServiceProvider()
+        {
+            _serviceProvider = new ServiceCollection()
+                .AddSingleton<ShellView>()
+                .AddSingleton<ShellViewModel>()
+                .BuildServiceProvider();
+
+        }
+    }
+}
