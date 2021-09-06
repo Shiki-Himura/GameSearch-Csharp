1c521ba3b66d0437ed2b7a82b98e78ff55b1c37e
Shiki-Himura <athaus@hotmail.de>
2021-09-06 15:21:34 +0200

    CleanUp

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -12,18 +12,34 @@ namespace GameSearch.ViewModels
 {
     public class ShellViewModel : BaseModel
     {
+        #region Private Fields(empty)
+
+        #endregion
+
+        #region Public Variables
+
         public Navigator Navigator { get; }
         public RelayCommand ShutDownCommand { get; set; }
+
+        #endregion
+
+        #region Methods
 
         public void ShutdownApp(object parameter)
         {
             System.Windows.Application.Current.Shutdown();
         }
 
+        #endregion
+
+        #region Constructor
+
         public ShellViewModel(Navigator navigator)
         {
             ShutDownCommand = new RelayCommand(ShutdownApp, null);
             Navigator = navigator;
         }
+
+        #endregion
     }
 }
